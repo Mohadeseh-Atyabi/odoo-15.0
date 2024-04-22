1d63a2f3e8ce4e1b0aed278a723b70a69ce86b94
Mohadeseh Atyabi <atyabi2000@gmail.com>
2024-04-22 17:08:40 +0330

    [Create] Hospital module

---
+++ custom_addons/hospital/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Hospital Management',
+    'version': '1.0.0',
+    'category': 'Hospital',
+    'sequence': -100,
+    'summary': 'Hospital Management System',
+    'description': """""",
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'assets': {},
+    'license': 'LGPL-3'
+}
