f6fed5d05a4f5bfc3ee7b56a15e9f3bd300e1f86
Mohadeseh Atyabi <atyabi2000@gmail.com>
2024-04-28 14:32:57 +0330

    [Update] Tracking for fields

--- custom_addons/hospital/models/patient.py
+++ custom_addons/hospital/models/patient.py
@@ -8,7 +8,7 @@ class HospitalPatient(models.Model):
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = "Hospital Patient"
 
-    name = fields.Char(string='Name')
+    name = fields.Char(string='Name', tracking=True)
     age = fields.Integer(string="Age")
     ref = fields.Char(string='Reference')
     gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string='Gender')
