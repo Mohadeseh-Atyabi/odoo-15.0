12ebc7af8b506d7eff4db41b2293e0c917b41bd0
Mohadeseh Atyabi <atyabi2000@gmail.com>
2024-04-28 10:07:49 +0330

    [Add] tree view to the patient model

--- custom_addons/hospital/models/patient.py
+++ custom_addons/hospital/models/patient.py
@@ -7,5 +7,5 @@ class HospitalPatient(models.Model):
     _description = "Hospital Patient"
 
     name = fields.Char(string='Name')
-    age = fields.Integer(string="age")
+    age = fields.Integer(string="Age")
     gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string='Gender')
