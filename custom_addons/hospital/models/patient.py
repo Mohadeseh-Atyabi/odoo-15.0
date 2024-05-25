4eb3e6413cce8eb4e876e4bd3a1c0567683c6153
Mohadeseh Atyabi <atyabi2000@gmail.com>
2024-05-25 14:01:01 +0330

    [Add] On the fly computed field (not saved in the database)

--- custom_addons/hospital/models/patient.py
+++ custom_addons/hospital/models/patient.py
@@ -1,4 +1,5 @@
 from odoo import api, fields, models, _, tools
+from datetime import date
 
 
 class HospitalPatient(models.Model):
@@ -9,8 +10,20 @@ class HospitalPatient(models.Model):
     _description = "Hospital Patient"
 
     name = fields.Char(string='Name', tracking=True)
-    age = fields.Integer(string="Age")
+    date_of_birth = fields.Date(string='Date of Birth')
+    # We cannot use a non-stored computed field in the search view
+    age = fields.Integer(string="Age", compute='_compute_age')
     ref = fields.Char(string='Reference')
     gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string='Gender')
     # By adding this field, we enable the model to be archived/unarchived by setting this field equal to True/False
     active = fields.Boolean(string="Active", default=True)
+
+    # Any changes in the date_of_birth field leads to executing this function
+    @api.depends('date_of_birth')
+    def _compute_age(self):
+        today = date.today()
+        for rec in self:
+            if rec.date_of_birth:
+                rec.age = today.year - rec.date_of_birth.year
+            else:
+                rec.age = 0
